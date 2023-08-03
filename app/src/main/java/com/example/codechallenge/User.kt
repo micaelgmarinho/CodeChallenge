d86ff3ec18658cb2d6558285d0d37f1aa7423be3
micaelgmarinho <micaelgarciamarinho@gmail.com>
2023-08-03 17:49:51 -0300

    add clean up

--- app/src/main/java/com/example/codechallenge/User.kt
+++ app/src/main/java/com/example/codechallenge/User.kt
@@ -1,6 +1,8 @@
 package com.example.codechallenge
 
-data class User(val id: Int,
-                val name: String,
-                val age: Int,
-                val email: String)
+data class User(
+    val id: Int,
+    val name: String,
+    val age: Int,
+    val email: String
+)
